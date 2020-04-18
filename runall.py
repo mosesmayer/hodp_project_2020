fdddb8bb346ad345b8444977367da1843b2db331
mosesmayer <mosesmayer@gmail.com>
2020-04-18 22:41:14 +0700

    added runall.py

---
+++ runall.py
@@ -0,0 +1,18 @@
+import sys, os
+import subprocess
+
+filenames = [
+    "Fall\\ 2016/fall_2016.xlsx",
+    "Fall\\ 2017/fall_2017.xlsx",
+    "Fall\\ 2018/fall_2018.xlsx",
+    "Fall\\ 2019/fall_2019.xlsx",
+    "Spring\\ 2016/spring_2016.xlsx",
+    "Spring\\ 2017/spring_2017.xlsx",
+    "Spring\\ 2018/spring_2018.xlsx",
+    "Spring\\ 2019/spring_2019.xlsx",
+]
+
+for i in filenames:
+    cmd = "python3 Excel_to_Pandas.py {}".format(i)
+    print(cmd)
+    subprocess.call(cmd, shell=True)
